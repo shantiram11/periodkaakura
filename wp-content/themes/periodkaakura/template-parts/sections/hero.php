069dc89380ccfa185f56ee7c7aebec3dc49cb45f
shantiram11 <shantiramtiwari0852@gmail.com>
2023-03-05 00:06:21 +0545

    pull from rikesh

--- wp-content/themes/periodkaakura/template-parts/sections/hero.php
+++ wp-content/themes/periodkaakura/template-parts/sections/hero.php
@@ -1,28 +1,27 @@
-
-    <section class="one">
-        <div class="talk">
-          <h1>Know about<br />period ka kura</h1>
-          <p>
+<section class="one container">
+    <div class="talk">
+        <h1>Know about<br/>period ka kura</h1>
+        <p>
             A space for menstruators to speak on the silenced subject "Period"
-          </p>
-          <span>Latest Episode: Period ka kura</span>
-          <div class="buttons">
+        </p>
+        <span>Latest Episode: Period ka kura</span>
+        <div class="buttons">
             <button><i class="fa-solid fa-play"></i>Listen Now</button>
             <button><i class="fa-solid fa-play"></i>Browse All</button>
-          </div>
-          <div id="subscribe">
+        </div>
+        <div id="subscribe">
             <span><i class="fa-solid fa-microphone" id="bord"></i>&nbsp&nbspSubscribe us</span>
 
-          </div>
-<div class="down">
-    <a><i class="fa-brands fa-apple"></i>Apple podcast</a>
-    <a><i class="fa-brands fa-soundcloud"></i>&nbspSound Cloud</a>
-    <a><i class="fa-solid fa-rss"></i>&nbspRSS Feed</a>
-</div>          
-        
         </div>
-        <div class="talkimage">
-          <img src="../image/pkk logo-02 1.png">
+        <div class="down">
+            <a><i class="fa-brands fa-apple"></i>Apple podcast</a>
+            <a><i class="fa-brands fa-soundcloud"></i>&nbspSound Cloud</a>
+            <a><i class="fa-solid fa-rss"></i>&nbspRSS Feed</a>
         </div>
 
-    </section>
+    </div>
+    <div class="talkimage">
+        <img src="../image/pkk logo-02 1.png">
+    </div>
+
+</section>
